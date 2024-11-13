7f9fbd74547fb9f4196734f918aa1d0bf4fc1057
Jonnibb1 <jonbekollii@gmail.com>
2024-11-13 17:49:55 +0100

    Update index.php
    dd

--- module8/index.php
+++ module8/index.php
@@ -1,2 +1,22 @@
 <?php
+//Creating a table using PDO
+$host = 'localhost';
+$db = 'db';
+$user = 'root';
+$pass = '';
+
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$db",$user,$pass);
+
+    $sql = "CREATE TABLE users(id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    username VARCHAR(20)NOT NULL,
+    password VARCHAR(20)NOT NULL)";
+$pdo -> exec($sql);
+
+echo "Table createt succesfully!!";
+
+}catch(exeption $e){
+    echo "Error while creating the table" .$e.getMessage();
+}
 ?>
