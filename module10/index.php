cbc5650d1596d28976ffcd78861cde6f819caa9f
Jonnibb1 <jonbekollii@gmail.com>
2024-11-27 17:48:13 +0100

    aaabrebir

---
+++ module10/index.php
@@ -0,0 +1,12 @@
+<?php
+include_once('config.php');
+
+$sql = "SELECT * FROM users";
+
+$getUsers = $connect ->prepare($sql);
+
+$getUsers->execute();
+
+$users = $getUsers->fetchAll();
+echo  var_dumb($users);
+?>
